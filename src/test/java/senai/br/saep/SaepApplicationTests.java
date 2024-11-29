fbfbea71aedf4b7ecd63d2e00ce4c26069af80a0
Carlos Correia <carlos.correia@sistemafiep.org.br>
2024-11-29 19:21:04 -0300

    SAEP

---
+++ src/test/java/senai/br/saep/SaepApplicationTests.java
@@ -0,0 +1,13 @@
+package senai.br.saep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
